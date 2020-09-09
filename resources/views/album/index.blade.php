b8f09ad999e4e211ce4041370983a55fe3938c3d
Corrado <corradoct@hotmail.it>
2020-09-09 17:30:32 +0200

    Version 0.3
    Ho aggiunto un po di grafica

--- resources/views/album/index.blade.php
+++ resources/views/album/index.blade.php
@@ -1,9 +1,28 @@
-@foreach ($albums as $album)
-  <li>
-    <a href="{{ route('album.show', $album) }}">
-      <img src="{{ $album->image->url }}" alt="image">
-      <h4>{{ $album->title }}</h4>
+@extends('layouts.layout')
 
-    </a>
-  </li>
-@endforeach
+@section('main_content')
+<div class="index">
+  <h1>Album</h1>
+
+  <div class="albums">
+
+    <ul>
+      @foreach ($albums as $album)
+        <li>
+          <a href="{{ route('album.show', $album) }}">
+            <div class="image">
+              <img src="{{ $album->image->url }}" alt="image">
+              <h4>{{ $album->title }}</h4>
+            </div>
+          </a>
+        </li>
+      @endforeach
+    </ul>
+
+  </div>
+
+  <a href="/">Home</a>
+</div>
+
+
+@endsection
--- resources/views/album/show.blade.php
+++ resources/views/album/show.blade.php
@@ -1,10 +1,30 @@
-<h1>Titolo: {{ $album->title }}</h1>
-<h4>Artista: {{ $album->artist }}</h4>
-<span>Anno: {{ $album->year }}</span>
+@extends('layouts.layout')
 
-<h2>Canzoni:</h2>
-<ul>
-  @foreach ($album->songs as $song)
-    <li>Titolo: {{ $song->title }} - Genere: {{ $song->genre }} - Durata: {{ $song->duration }}</li>
-  @endforeach
-</ul>
+@section('main_content')
+  <div class="songView">
+
+    <div class="image">
+      <img src="{{ $album->image->url }}" alt="image">
+    </div>
+    <div class="details">
+      <h1>{{ $album->title }}</h1>
+      <h4>Artista: {{ $album->artist }}</h4>
+      <span>Anno: {{ $album->year }}</span>
+    </div>
+
+    <div class="songs">
+      <h2>Canzoni:</h2>
+      <div class="songsList">
+        <ul>
+          @foreach ($album->songs as $song)
+            <li><span>Titolo:</span> {{ $song->title }} - <span>Genere:</span> {{ $song->genre }} - <span>Durata:</span> {{ $song->duration }}</li>
+          @endforeach
+        </ul>
+      </div>
+    </div>
+
+    <a href="{{ route('album.index') }}">Indietro</a>
+  </div>
+
+
+@endsection
